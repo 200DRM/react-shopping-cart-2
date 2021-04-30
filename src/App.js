43d2f2c54d7ce9ad8474cf37e2f1afdf9f779cfd
200DRM <dagmar.prokonsul@gmail.com>
2021-04-30 13:06:17 +0200

    components react

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
+import bootstrap from '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+import { 
+  BrowserRouter,
+  Route,
+  Switch
+} from 'react-router-dom';
+
+import AddItem from './components/AddItem';
+import Cart from './components/Cart';
+import Items from './components/Items';
+import Navbar from './components/Navbar';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
+      <BrowserRouter>
+        <Switch>
+          <Route path='/' component={Items} exact />
+          <Route path='/add-item' component={AddItem} />
+          <Route path='/cart' component={Cart} exact />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,28 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div>
-      Navbar
+      <nav class='navbar navbar-expand-lg'>
+        <a class='navbar-brand' href='/'>Navbar</a>
+        <button class='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarSupportedContent' aria-controls='navbarSupportedContent' aria-expanded='false' aria-label='Toggle navigation'>
+          <span class='navbar-toggler-icon'></span>
+        </button>
+
+        <div class='collapse navbar-collapse' id='navbarSupportedContent'>
+          <ul class='navbar-nav mx-auto'>
+            <li class='nav-item'>
+              <a class='nav-link' href='/'>Home</a>
+            </li>
+            <li class='nav-item'>
+              <a class='nav-link' href='/add-item'>Add Item</a>
+            </li>
+          </ul>
+          <ul class='navbar-nav mx-right'>
+            <li class='nav-item'>
+              <a class='nav-link' href='/cart'>Cart</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
     </div>
   )
 }
